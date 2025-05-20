bfc321f92e9237125f125ed8f5f967014f980a26
Midhun Eldose <midhun.eldose@neura-robotics.com>
2025-05-20 11:39:43 +0200

    test_motion_planning_client.py

--- test_motion_planning_client.py
+++ test_motion_planning_client.py
@@ -6,23 +6,23 @@ from neura_ai_robot_api.utils.types import SolidPrimitive
 from neurapy_ai.utils.types import Pose, JointState
 from sensor_msgs.msg import Image, PointCloud2
 
-# calliing the main function
+# calling the main function
 
 if __name__ == "__main__" :
-    rospy.init_node("test_motion_planning")
-    PSC = MotionPlanningClient()
+    rospy.init_node("test_motion_planning") # initialise the node 
+    PSC = MotionPlanningClient() # setting up the class MotionPlanningClient
 
     param = PSC.PlannerParam()
     param.wait_for_user_confirm = True
 
-    PSC.update_planning_params(param)
+    PSC.update_planning_params(param) # updating the plannig params
     
-    goal_pose = Pose([1.0,0.0,1.0],[-math.pi,0.0,-math.pi])
-    goal_pose2 = Pose([1.0,0.0,0.5],[-math.pi,0.0,-math.pi])
+    goal_pose = Pose([1.0,0.0,1.0],[-math.pi,0.0,-math.pi]) # goal pose
+    goal_pose2 = Pose([1.0,0.0,0.5],[-math.pi,0.0,-math.pi]) # goal pose 2
     joint_pose=JointState([100*math.pi/180,25*math.pi/180,160*math.pi/180,-75*math.pi/180,9*math.pi/180,-78*math.pi/180,-96*math.pi/180])
 
-    joint_pose1=[103*math.pi/180,25*math.pi/180,160*math.pi/180,-75*math.pi/180,9*math.pi/180,-78*math.pi/180,-96*math.pi/180]
-    joint_pose2=[24*math.pi/180,33*math.pi/180,-11*math.pi/180,48*math.pi/180,-1*math.pi/180,99*math.pi/180,6*math.pi/180]
+    joint_pose1=[103*math.pi/180,25*math.pi/180,160*math.pi/180,-75*math.pi/180,9*math.pi/180,-78*math.pi/180,-96*math.pi/180] # setting the jointpose1
+    joint_pose2=[24*math.pi/180,33*math.pi/180,-11*math.pi/180,48*math.pi/180,-1*math.pi/180,99*math.pi/180,6*math.pi/180]# setting the jointpose2
     joint_poses = [joint_pose1, joint_pose2]
 
     states = PSC.get_current_joint_states()
