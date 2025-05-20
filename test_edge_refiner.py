37188440c4f8c7895e01206114d04861166f7c8a
Midhun Eldose <midhun.eldose@neura-robotics.com>
2025-05-20 17:00:42 +0200

    test_edge_refiner.py

--- test_edge_refiner.py
+++ test_edge_refiner.py
@@ -1,5 +1,7 @@
 from neura_ai_robot_api.deprecated_clients.edge_refiner_client import EdgeRefinerClient
 
+
+# calling the main function
 if __name__ == '__main__':
     detect_marker_client = EdgeRefinerClient()
     beg = [0, 0, 1, 0, 0, 0, 1]
